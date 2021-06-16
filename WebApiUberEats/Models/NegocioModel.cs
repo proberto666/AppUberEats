2eb70ee862aa38f26467601e107903b270f2557f
proberto666 <65254723+proberto666@users.noreply.github.com>
2021-06-15 19:31:47 -0500

    Cambios en el connection string

--- WebApiUberEats/Models/NegocioModel.cs
+++ WebApiUberEats/Models/NegocioModel.cs
@@ -7,8 +7,6 @@ namespace WebApiUberEats.Models
 {
     public class NegocioModel
     {
-        string ConnectionString = "Server=tcp:river-db.database.windows.net,1433;Initial Catalog=UberEatsDB;Persist Security Info=False;User ID=rhy70705;Password=Admin1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-
         public int NumNegocio { get; set; }
 
         public string Usuario { get; set; }
